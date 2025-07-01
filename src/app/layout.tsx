1f92455449bf5904d8af44c744e73bb7dc4d2ba6
Arjun Gulyani <gulyaniarjun@gmail.com>
2025-07-01 17:12:09 +0000

    favicon on browser tab still showed firebase studio icon. logo on header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,7 @@ export const metadata: Metadata = {
   title: 'Circana | AS Errors Dashboard',
   description: 'Errors from AS Logs DB',
   icons: {
-    icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
+    icon: '/favicon-32x32.png',
   },
 };
 
