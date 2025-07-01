1f43c6e2b84b89bd6d038e36ab7337682856593c
Arjun Gulyani <gulyaniarjun@gmail.com>
2025-07-01 17:28:45 +0000

    it's better but completely solid now, the overallping circles or elipses

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,13 +7,13 @@ const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <g>
-      <ellipse fill="#582D83" cx="45" cy="50" rx="20" ry="40" />
-      <ellipse fill="#75308A" cx="50" cy="50" rx="20" ry="40" />
-      <ellipse fill="#933491" cx="55" cy="50" rx="20" ry="40" />
-      <ellipse fill="#B03898" cx="60" cy="50" rx="20" ry="40" />
-      <ellipse fill="#D34494" cx="65" cy="50" rx="20" ry="40" />
-      <ellipse fill="#E96684" cx="70" cy="50" rx="20" ry="40" />
-      <ellipse fill="#F58575" cx="75" cy="50" rx="20" ry="40" />
+      <ellipse fill="#582D83" cx="45" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#75308A" cx="50" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#933491" cx="55" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#B03898" cx="60" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#D34494" cx="65" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#E96684" cx="70" cy="50" rx="20" ry="40" fillOpacity="0.85" />
+      <ellipse fill="#F58575" cx="75" cy="50" rx="20" ry="40" fillOpacity="0.85" />
     </g>
   </svg>
 );
