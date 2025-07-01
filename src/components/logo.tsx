1fd7ba07361dae8e29918a9f8d53e61e89ba61fd
Arjun Gulyani <gulyaniarjun@gmail.com>
2025-07-01 17:29:24 +0000

    leave it, just use some generic image

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,8 @@
+import { ShieldAlert } from 'lucide-react';
 import * as React from 'react';
 
 const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    viewBox="0 0 100 100"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <g>
-      <ellipse fill="#582D83" cx="45" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#75308A" cx="50" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#933491" cx="55" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#B03898" cx="60" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#D34494" cx="65" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#E96684" cx="70" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-      <ellipse fill="#F58575" cx="75" cy="50" rx="20" ry="40" fillOpacity="0.85" />
-    </g>
-  </svg>
+  <ShieldAlert {...props} />
 );
 
 export default Logo;
