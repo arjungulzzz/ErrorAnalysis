21024ae71ac9b936fd93775d6083128e088484d6
Arjun Gulyani <gulyaniarjun@gmail.com>
2025-07-02 04:27:12 +0000

    I've added favicon.ico but logo in header still not using that

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,14 @@
-import { ShieldAlert } from 'lucide-react';
+import Image from 'next/image';
 import * as React from 'react';
 
-const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <ShieldAlert {...props} />
+const Logo = (props: { className?: string }) => (
+  <Image
+    src="/favicon.ico"
+    alt="Application Logo"
+    width={40}
+    height={40}
+    {...props}
+  />
 );
 
 export default Logo;
