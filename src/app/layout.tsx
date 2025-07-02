d34152b11f5b50b314a391fa916927740acc2c90
Arjun Gulyani <gulyaniarjun@gmail.com>
2025-07-02 04:55:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ export const metadata: Metadata = {
   title: 'Circana | AS Errors Dashboard',
   description: 'Errors from AS Logs DB',
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/favicon.ico' },
+      { url: "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 24 24' fill='%2359459e' stroke='%23fafafa' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpath d='m21.73 18-8-14a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z'/%3e%3cpath d='M12 9v4'/%3e%3cpath d='M12 17h.01'/%3e%3c/svg%3e", type: 'image/svg+xml' }
+    ],
   },
 };
 
